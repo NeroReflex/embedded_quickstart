6a12820e48f9bf475b76562245335d06e287b9a2
NeroReflex <benato.denis96@gmail.com>
2025-02-10 16:11:00 +0000

    Auto-start login manager

--- package/greetd/greetd.mk
+++ package/greetd/greetd.mk
@@ -3,5 +3,16 @@ GREETD_LICENSE = GPLv3
 GREETD_SITE = $(call github,kennylevinsen,greetd,$(GREETD_VERSION))
 GREETD_DEPENDENCIES = host-rustc
 GREETD_SUBDIR = greetd
+LOGIN_NG_INSTALL_STAGING = YES
+
+define GREETD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/greetd.service $(TARGET_DIR)/lib/systemd/system/greetd.service
+endef
+
+define GREETD_POST_INSTALL
+	$(INSTALL) -D -m 644 $(@D)/config.toml $(TARGET_DIR)/etc/greetd/config.toml
+endef
+
+GREETD_POST_INSTALL_TARGET_HOOKS += GREETD_POST_INSTALL
 
 $(eval $(cargo-package))
--- package/login_ng/login_ng.mk
+++ package/login_ng/login_ng.mk
@@ -2,5 +2,12 @@ LOGIN_NG_VERSION = 0.1.6
 LOGIN_NG_LICENSE = GPLv2
 LOGIN_NG_SITE = $(call github,NeroReflex,login-ng,$(LOGIN_NG_VERSION))
 LOGIN_NG_DEPENDENCIES = host-rustc
+LOGIN_NG_INSTALL_STAGING = YES
+
+define LOGIN_NG_FIX_STAGING_GREETD_CONFIG
+	$(SED) 's|agreety --cmd /bin/sh|/usr/bin/login-ng_cli|' $(TARGET_DIR)/etc/greetd/config.toml
+endef
+
+LOGIN_NG_POST_INSTALL_STAGING_HOOKS += LOGIN_NG_FIX_STAGING_GREETD_CONFIG
 
 $(eval $(cargo-package))
