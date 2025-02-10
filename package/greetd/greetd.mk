0833a460caad9ad04c47b3919f9502c85256bc7c
NeroReflex <benato.denis96@gmail.com>
2025-02-10 13:27:46 +0000

    Fix virtual workspace error in greetd

--- package/greetd/greetd.mk
+++ package/greetd/greetd.mk
@@ -2,5 +2,6 @@ GREETD_VERSION = 0.10.3
 GREETD_LICENSE = GPLv3
 GREETD_SITE = $(call github,kennylevinsen,greetd,$(GREETD_VERSION))
 GREETD_DEPENDENCIES = host-rustc
+GREETD_SUBDIR = greetd
 
 $(eval $(cargo-package))
