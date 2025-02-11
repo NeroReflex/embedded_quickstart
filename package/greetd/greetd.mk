94c6d650044847fec738b5cd6be3fc9720839a3d
NeroReflex <benato.denis96@gmail.com>
2025-02-11 10:47:56 +0000

    license: use correct names

--- package/greetd/greetd.mk
+++ package/greetd/greetd.mk
@@ -1,5 +1,5 @@
 GREETD_VERSION = 0.10.3
-GREETD_LICENSE = GPLv3
+GREETD_LICENSE = GPL-3.0
 GREETD_SITE = $(call github,kennylevinsen,greetd,$(GREETD_VERSION))
 GREETD_DEPENDENCIES = host-rustc
 GREETD_SUBDIR = greetd
