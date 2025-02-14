6857fee13b0734b94b67b7844494a9c393d1a3fa
NeroReflex <benato.denis96@gmail.com>
2025-02-14 13:37:37 +0000

    Better version handling

--- package/autologin/autologin.mk
+++ package/autologin/autologin.mk
@@ -1,5 +1,6 @@
+AUTOLOGIN_VERSION = $(LOGIN_NG_VERSION)
 AUTOLOGIN_LICENSE = GPL-2.0-or-later
-AUTOLOGIN_SITE = $(call github,NeroReflex,login-ng,$(LOGIN_NG_VERSION))
+AUTOLOGIN_SITE = $(call github,NeroReflex,login-ng,$(AUTOLOGIN_VERSION))
 AUTOLOGIN_DEPENDENCIES = host-rustc
 AUTOLOGIN_SUBDIR = login_ng-ctl
 
