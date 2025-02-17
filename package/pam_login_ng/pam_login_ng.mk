1e7a5c401b77d22da728370c393fe80fca58da0c
NeroReflex <benato.denis96@gmail.com>
2025-02-17 17:12:16 +0000

    Add the service to pam

--- package/pam_login_ng/pam_login_ng.mk
+++ package/pam_login_ng/pam_login_ng.mk
@@ -3,15 +3,21 @@ PAM_LOGIN_NG_LICENSE = GPL-2.0-or-later
 PAM_LOGIN_NG_SITE = $(call github,NeroReflex,login-ng,$(PAM_LOGIN_NG_VERSION))
 PAM_LOGIN_NG_DEPENDENCIES = host-rustc
 PAM_LOGIN_NG_SUBDIR = pam_login_ng
-#PAM_LOGIN_NG_INSTALL_STAGING = YES
-#
-#define PAM_LOGIN_NG_POST_INSTALL
-#	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_EMBEDDED_QUICKSTART_PATH)/package/login_ng-ctl/login_ng-ctl.pam $(TARGET_DIR)/etc/pam.d/login_ng-ctl
-#endef
-#
-#PAM_LOGIN_NG_POST_INSTALL_TARGET_HOOKS += PAM_LOGIN_NG_POST_INSTALL
+PAM_LOGIN_NG_INSTALL_STAGING = YES
+
 # ls -lah /usr say this: lib64 -> lib
 # so install the pam module to /usr/lib/security
 #
+define PAM_LOGIN_NG_INSTALL_PAM_MODULE
+	$(INSTALL) -D -m 755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/libpam_login_ng.so \
+		$(TARGET_DIR)/usr/lib/security/pam_login_ng.so
+endef
+
+define PAM_LOGIN_NG_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/rootfs/usr/lib/systemd/system/pam_login_ng.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/pam_login_ng.service
+endef
+
+PAM_LOGIN_NG_POST_INSTALL_TARGET_HOOKS += PAM_LOGIN_NG_INSTALL_PAM_MODULE
 
 $(eval $(cargo-package))
