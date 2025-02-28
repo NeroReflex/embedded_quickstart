d1795ab0989b552770926088de402da6d0f7ef11
Denis Benato <benato.denis96@gmail.com>
2025-02-28 16:34:06 +0000

    Improve imx-atf

--- package/imx-atf/imx-atf.mk
+++ package/imx-atf/imx-atf.mk
@@ -1,11 +1,20 @@
 # Define the package name and version
-IMX_ATF_NAME := mysoftware
+IMX_ATF_NAME := imx-atf
 IMX_ATF_VERSION := 1.0
 IMX_ATF_SITE = $(call github,nxp-imx,imx-atf,lf-6.12.3-imx943-er1)
 
+IMX_ATF_MAKE_ENV = \
+        $(HOST_MAKE_ENV) \
+        BR_BINARIES_DIR=$(BINARIES_DIR)
+
+IMX_ATF_DEPENDENCIES = \
+        host-kmod \
+        $(BR2_MAKE_HOST_DEPENDENCY)
+IMX_ATF_MAKE = $(BR2_MAKE)
+
 # Define the target
 define IMX_ATF_BUILD_CMDS
-    $(MAKE) PLAT=$(BR2_EQ_IMX_ATF_PLAT) bl31
+    $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) PLAT=$(BR2_EQ_IMX_ATF_PLAT) CROSS_COMPILE="$(TARGET_CROSS)" bl31
 endef
 
 ## Define the install commands
