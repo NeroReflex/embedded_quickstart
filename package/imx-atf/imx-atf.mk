4334787753e90dcf7841c3b309d114b641e78188
Denis Benato <benato.denis96@gmail.com>
2025-03-03 15:39:36 +0000

    copy imx-atf

--- package/imx-atf/imx-atf.mk
+++ package/imx-atf/imx-atf.mk
@@ -3,6 +3,8 @@ IMX_ATF_NAME := imx-atf
 IMX_ATF_VERSION := 1.0
 IMX_ATF_SITE = $(call github,nxp-imx,imx-atf,lf-6.12.3-imx943-er1)
 
+IMX_ATF_FILENAME=$(BR2_EQ_IMX_ATF_TARGET).bin
+
 IMX_ATF_MAKE_ENV = \
         $(HOST_MAKE_ENV) \
         BR_BINARIES_DIR=$(BINARIES_DIR)
@@ -18,9 +20,9 @@ define IMX_ATF_BUILD_CMDS
 endef
 
 ## Define the install commands
-#define IMX_ATF_INSTALL_CMDS
-#    $(INSTALL) -D -m 0755 mysoftware $(TARGET_DIR)/usr/bin/mysoftware
-#endef
+define IMX_ATF_INSTALL_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/build/$(BR2_EQ_IMX_ATF_PLAT)/release/$(IMX_ATF_FILENAME) $(HOST_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME)
+endef
 
 # Package definition
 $(eval $(generic-package))
