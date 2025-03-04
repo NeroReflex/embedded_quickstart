76dea63d1cc49603dae2aa64026ad255bcefa593
Denis Benato <benato.denis96@gmail.com>
2025-03-04 13:23:56 +0000

    Writing to the terminal attempt to execute the command. lol

--- package/imx-mkimage-post/imx-mkimage-post.mk
+++ package/imx-mkimage-post/imx-mkimage-post.mk
@@ -22,13 +22,13 @@ define HOST_IMX_MKIMAGE_POST_BUILD_CMDS
     $(shell find $(BINARIES_DIR) -type f -name "*.bin" -exec cp {} $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/ \;) \
     $(shell cp $(BUILD_DIR)/firmware-imx-*/firmware/ddr/synopsys/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(shell find $(BUILD_DIR)/uboot-* -type f -name "*.dtb" -exec cp {} $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/ \;) \
-    $(shell cp -v $(TARGET_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME) $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
+    $(shell cp $(TARGET_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME) $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) MKIMAGE=$(HOST_DIR)/bin/mkimage $(MAKE) -C $(@D) SOC=$(BR2_EQ_IMX_MKIMAGE_POST_SOC) dtbs=$(BR2_EQ_IMX_MKIMAGE_POST_DTBS) $(BR2_EQ_IMX_MKIMAGE_POST_TARGET)
 endef
 
 # Define the install commands
 define IMX_MKIMAGE_POST_INSTALL_CMDS
-    $(shell cp -v $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/flash.bin $(BUILD_DIR)/boot-imx) \
+    $(shell cp $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/flash.bin $(BUILD_DIR)/boot-imx) \
     $(INSTALL) -D -m 0755 mkimage_imx8 $(HOST_DIR)/usr/bin/mkimage_imx8
 endef
 
