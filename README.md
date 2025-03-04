422dd90c289ebb145716fd806a9968a978400ca6
Denis Benato <benato.denis96@gmail.com>
2025-03-04 10:40:09 +0000

    Improve imx support

--- README.md
+++ README.md
@@ -22,7 +22,13 @@ It is also suggested that you __disable__ *the remount rootfs as r/w*, and manag
 
 The following is bootloader-specific configurations: choose based on your bootloader of preference.
 
+__WARNING__ The bootloader __MUST__ support BTRFS: in case of u-boot this is ensured by the configuration fragment.
+
 ### U-Boot
 
 If you use u-boot you have to include the *uboot.conf* file to *Additional configuration fragment files* in
 bootloaders -> u-boot.
+
+__NOTE__ when generating a target for i.MX8 make sure to create u-boot-nodtb.bin (it is a file created when building u-boot.bin)
+and it only needs to be specified as an additional u-boot binary format to be copied into BINARIES_DIR and therefore picked up
+by mkimage.
--- package/imx-mkimage-post/imx-mkimage-post.mk
+++ package/imx-mkimage-post/imx-mkimage-post.mk
@@ -1,6 +1,6 @@
 # Define the package name and version
 IMX_MKIMAGE_POST_NAME := imx-mkimage-post
-IMX_MKIMAGE_POST_VERSION := 5.11
+IMX_MKIMAGE_POST_VERSION := 5.12
 IMX_MKIMAGE_POST_SITE = $(call github,NeroReflex,imx-mkimage,$(IMX_MKIMAGE_POST_VERSION))
 
 IMX_MKIMAGE_POST_DEPENDENCIES = \
@@ -19,10 +19,8 @@ IMX_MKIMAGE_POST_MAKE = $(BR2_MAKE)
 # Define the target
 define HOST_IMX_MKIMAGE_POST_BUILD_CMDS
     $(shell cp $(BINARIES_DIR)/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
-    $(shell cp $(BINARIES_DIR)/u-boot.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/u-boot-nodtb.bin) \
     $(shell cp $(TARGET_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME) $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
-    $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) MKIMAGE=$(HOST_DIR)/bin/mkimage $(MAKE) -C $(@D) SOC=$(BR2_EQ_IMX_MKIMAGE_POST_SOC) $(BR2_EQ_IMX_MKIMAGE_POST_TARGET) \
-    $(shell rm ./mkimage_uboot)
+    $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) MKIMAGE=$(HOST_DIR)/bin/mkimage $(MAKE) -C $(@D) SOC=$(BR2_EQ_IMX_MKIMAGE_POST_SOC) $(BR2_EQ_IMX_MKIMAGE_POST_TARGET)
 endef
 
 # Define the install commands
