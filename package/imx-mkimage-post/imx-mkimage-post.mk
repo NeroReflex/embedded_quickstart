d3fc22d37559a713b654e6decf1078b835348e98
Denis Benato <benato.denis96@gmail.com>
2025-03-04 12:32:14 +0000

    copy firmware files

--- package/imx-mkimage-post/imx-mkimage-post.mk
+++ package/imx-mkimage-post/imx-mkimage-post.mk
@@ -6,7 +6,8 @@ IMX_MKIMAGE_POST_SITE = $(call github,NeroReflex,imx-mkimage,$(IMX_MKIMAGE_POST_
 IMX_MKIMAGE_POST_DEPENDENCIES = \
         imx-atf \
         u-boot \
-        host-uboot-tools
+        host-uboot-tools \
+        firmware-imx
 
 IMX_MKIMAGE_POST_MAKE_ENV = \
         $(HOST_MAKE_ENV) \
@@ -19,6 +20,7 @@ IMX_MKIMAGE_POST_MAKE = $(BR2_MAKE)
 # Define the target
 define HOST_IMX_MKIMAGE_POST_BUILD_CMDS
     $(shell cp $(BINARIES_DIR)/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
+    $(shell cp $(BUILD_DIR)/firmware-imx-*/firmware/ddr/synopsys/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(shell cp $(TARGET_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME) $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) MKIMAGE=$(HOST_DIR)/bin/mkimage $(MAKE) -C $(@D) SOC=$(BR2_EQ_IMX_MKIMAGE_POST_SOC) $(BR2_EQ_IMX_MKIMAGE_POST_TARGET)
 endef
