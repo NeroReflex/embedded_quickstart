782bd8cf17bff493efa67efea5b2ce0ab067dfbd
Denis Benato <benato.denis96@gmail.com>
2025-03-04 13:19:10 +0000

    Remove broken echo

--- package/imx-mkimage-post/imx-mkimage-post.mk
+++ package/imx-mkimage-post/imx-mkimage-post.mk
@@ -19,13 +19,9 @@ IMX_MKIMAGE_POST_MAKE = $(BR2_MAKE)
 
 # Define the target
 define HOST_IMX_MKIMAGE_POST_BUILD_CMDS
-    @echo "Copy u-boot compiled binaries into mkimage directory" \
-    $(shell cp -v $(BINARIES_DIR)/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
-    @echo "Copy firmware files into mkimage directory" \
-    $(shell cp -v $(BUILD_DIR)/firmware-imx-*/firmware/ddr/synopsys/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
-    @"Copy u-boot dtb files into mkimage directory" \
+    $(shell find $(BINARIES_DIR) -type f -name "*.bin" -exec cp {} $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/ \;) \
+    $(shell cp $(BUILD_DIR)/firmware-imx-*/firmware/ddr/synopsys/*.bin $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(shell find $(BUILD_DIR)/uboot-* -type f -name "*.dtb" -exec cp {} $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/ \;) \
-    @"Copy imx-atf into mkimage directory" \
     $(shell cp -v $(TARGET_DIR)/usr/lib/firmware/$(IMX_ATF_FILENAME) $(@D)/$(BR2_EQ_IMX_MKIMAGE_POST_SOC_DIR)/) \
     $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) MKIMAGE=$(HOST_DIR)/bin/mkimage $(MAKE) -C $(@D) SOC=$(BR2_EQ_IMX_MKIMAGE_POST_SOC) dtbs=$(BR2_EQ_IMX_MKIMAGE_POST_DTBS) $(BR2_EQ_IMX_MKIMAGE_POST_TARGET)
 endef
