cb24a49e225f622f9f1b676c5889482f7cced72b
Denis Benato <benato.denis96@gmail.com>
2025-03-07 13:39:18 +0000

    Fix bootloader write

--- post_rootfs.sh
+++ post_rootfs.sh
@@ -45,9 +45,9 @@ if [ -f "${BUILD_DIR}/image_path" ] && [ -f "${BUILD_DIR}/image_part" ]; then
     IMAGE_FILE_PATH=$(cat "${BUILD_DIR}/image_path")
     IMAGE_PART_NUMBER=$(cat "${BUILD_DIR}/image_part")
 
-    if [ -f "${BUILD_DIR}boot-imx" ]; then
+    if [ -f "${BUILD_DIR}/boot-imx" ]; then
         echo "Writing the bootloader"
-        if ! dd if="${BUILD/DIR}boot-imx" of="${IMAGE_FILE_PATH}" bs=1K seek=33 conv=fsync ; then
+        if ! dd if="${BUILD_DIR}/boot-imx" of="${IMAGE_FILE_PATH}" bs=1K seek=33 conv=fsync ; then
             echo "ERROR: Could not write boot-imx to image"
             exit -1
         fi
