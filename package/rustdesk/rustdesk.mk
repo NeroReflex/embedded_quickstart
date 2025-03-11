eb764aac04049ada5c7d1a098c80c8b30a74f724
Denis Benato <benato.denis96@gmail.com>
2025-03-11 13:28:37 +0000

    fix rustdesk download

--- package/rustdesk/rustdesk.mk
+++ package/rustdesk/rustdesk.mk
@@ -1,7 +1,8 @@
 RUSTDESK_VERSION = 1.3.8
 RUSTDESK_LICENSE = AGPL-3.0
-RUSTDESK_SITE = $(call github,rustdesk,rustdesk,$(RUSTDESK_VERSION))
+RUSTDESK_SITE_METHOD = git
 RUSTDESK_GIT_SUBMODULES = YES
+RUSTDESK_SITE = https://github.com/rustdesk/rustdesk.git
 RUSTDESK_DEPENDENCIES = linux-pam \
 	host-rustc
 
