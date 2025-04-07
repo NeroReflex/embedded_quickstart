21bb02ae182b57542b81944f921df359faf2ad95
Denis Benato <benato.denis96@gmail.com>
2025-04-07 15:14:51 +0000

    Change position of the executable link

--- package/atombutter/atombutter.mk
+++ package/atombutter/atombutter.mk
@@ -8,7 +8,7 @@ ATOMBUTTER_CARGO_BUILD_OPTS = --all-features
 ATOMBUTTER_CARGO_INSTALL_OPTS = --all-features
 
 define ATOMBUTTER_POST_INSTALL
-	$(shell ln -sf /usr/bin/atombutter ${TARGET_DIR}/etc/tab)
+	$(shell ln -sf /usr/bin/atombutter ${TARGET_DIR}/usr/bin/init)
 endef
 
 ATOMBUTTER_POST_INSTALL_POST_INSTALL_TARGET_HOOKS += ATOMBUTTER_POST_INSTALL
