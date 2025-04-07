ccc66f6f91945d9a85bdc5ff805acfbe20001dcd
Denis Benato <benato.denis96@gmail.com>
2025-04-07 13:35:53 +0000

    fix download filename

--- package/atombutter/atombutter.mk
+++ package/atombutter/atombutter.mk
@@ -1,6 +1,7 @@
-ATOMBUTTER_VERSION = 0.1.2
+ATOMBUTTER_VERSION = 0.1.3
 ATOMBUTTER_LICENSE = GPL-2.0-or-later
-ATOMBUTTER_SITE = https://github.com/NeroReflex/AtomButter/archive/refs/tags/$(ATOMBUTTER_VERSION).tar.gz
+ATOMBUTTER_SOURCE = $(ATOMBUTTER_VERSION).tar.gz
+ATOMBUTTER_SITE = https://github.com/NeroReflex/AtomButter/archive/refs/tags/
 ATOMBUTTER_DEPENDENCIES = host-rustc
 ATOMBUTTER_INSTALL_STAGING = YES
 ATOMBUTTER_CARGO_BUILD_OPTS = --all-features
