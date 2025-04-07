db63f58ddd186d9eae7e83532798e4bc3737a523
Denis Benato <benato.denis96@gmail.com>
2025-04-07 16:07:13 +0000

    Update stuPID1

--- package/stupid1/stupid1.mk
+++ package/stupid1/stupid1.mk
@@ -1,4 +1,4 @@
-STUPID1_VERSION = 1.1.0
+STUPID1_VERSION = 1.1.1
 STUPID1_LICENSE = GPL-2.0-or-later
 STUPID1_SOURCE = $(STUPID1_VERSION).tar.gz
 STUPID1_SITE = https://github.com/NeroReflex/stuPID1/archive/refs/tags
