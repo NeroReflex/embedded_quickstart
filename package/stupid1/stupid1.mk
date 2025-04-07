46a3f6c710bc88bcca39280f3fbe4f9f4e26a9e1
Denis Benato <benato.denis96@gmail.com>
2025-04-07 15:48:59 +0000

    update stuPID1

--- package/stupid1/stupid1.mk
+++ package/stupid1/stupid1.mk
@@ -1,7 +1,9 @@
-STUPID1_VERSION = 0.1.1
+STUPID1_VERSION = 1.1.0
 STUPID1_LICENSE = GPL-2.0-or-later
 STUPID1_SOURCE = $(STUPID1_VERSION).tar.gz
 STUPID1_SITE = https://github.com/NeroReflex/stuPID1/archive/refs/tags
 STUPID1_DEPENDENCIES = host-rustc
+STUPID1_CARGO_BUILD_OPTS = --all-features
+STUPID1_CARGO_INSTALL_OPTS = --all-features
 
 $(eval $(cargo-package))
