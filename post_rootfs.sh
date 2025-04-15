ab389b145f8ea0582e65c06a0559db18647eee31
Denis Benato <benato.denis96@gmail.com>
2025-04-15 08:59:56 +0000

    Remove useless file

--- post_rootfs.sh
+++ post_rootfs.sh
@@ -375,7 +375,7 @@ overlay               /sysroot/etc  overlay  rw,noatime,noatime,lowerdir=/sysroo
 overlay               /sysroot/var  overlay  rw,noatime,noatime,lowerdir=/sysroot/var,upperdir=/sysroot/base/${DEPLOYMENTS_DATA_DIR}/${DEPLOYMENT_SUBVOL_NAME}/var_overlay/upperdir,workdir=/sysroot/base/${DEPLOYMENTS_DATA_DIR}/${DEPLOYMENT_SUBVOL_NAME}/var_overlay/workdir,index=off,metacopy=off,xino=off,redirect_dir=off 0 0
 " | sudo tee "${EXTRACTED_ROOTFS_HOST_PATH}/etc/rdtab"
 
-echo "${DEPLOYMENT_SUBVOL_NAME}" | sudo tee "${EXTRACTED_ROOTFS_HOST_PATH}/etc/rdname"
+#echo "${DEPLOYMENT_SUBVOL_NAME}" | sudo tee "${EXTRACTED_ROOTFS_HOST_PATH}/etc/rdname"
 
 echo "Sealing the BTRFS subvolume containing the rootfs"
 
