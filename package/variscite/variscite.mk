50e973948f1f9833c12de8d6aa31dcb98018a806
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-06-17 11:42:55 +0200

    Add all required scripts to support var-som-imx8mm

--- package/variscite/variscite.mk
+++ package/variscite/variscite.mk
@@ -7,6 +7,26 @@ define VARISCITE_INSTALL_GPIOCHIP
 	$(INSTALL) -D -m 644 $(@D)/gpiochip $(TARGET_DIR)/etc/gpiochip
 endef
 
-VARISCITE_POST_INSTALL_TARGET_HOOKS += VARISCITE_INSTALL_GPIOCHIP
+define VARISCITE_INSTALL_WIRELESS
+	$(INSTALL) -D -m 755 $(@D)/variscite-wireless $(TARGET_DIR)/etc/wifi/variscite-wireless
+endef
+
+define VARISCITE_INSTALL_WIFI
+	$(INSTALL) -D -m 755 $(@D)/variscite-wifi $(TARGET_DIR)/etc/wifi/variscite-wifi
+endef
+
+define VARISCITE_INSTALL_BLUETOOTH
+	$(INSTALL) -D -m 755 $(@D)/variscite-bt $(TARGET_DIR)/etc/bluetooth/variscite-bt
+endef
+
+define VARISCITE_INSTALL_WIFI_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/variscite-wifi.service $(TARGET_DIR)/lib/systemd/system/variscite-wifi.service
+endef
+
+define VARISCITE_INSTALL_BLUETOOTH_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/variscite-bt.service $(TARGET_DIR)/lib/systemd/system/variscite-bt.service
+endef
+
+VARISCITE_POST_INSTALL_TARGET_HOOKS += VARISCITE_INSTALL_GPIOCHIP VARISCITE_INSTALL_WIRELESS VARISCITE_INSTALL_WIFI VARISCITE_INSTALL_BLUETOOTH VARISCITE_INSTALL_WIFI_SYSTEMD VARISCITE_INSTALL_BLUETOOTH_SYSTEMD
 
 $(eval $(generic-package))
