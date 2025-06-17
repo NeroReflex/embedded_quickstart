4f8bb8e6bad4d5a2015b89b55a7b90374ded2826
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-06-17 13:57:57 +0200

    fix misplaced files

--- package/var_som_imx8mm/var_som_imx8mm.mk
+++ package/var_som_imx8mm/var_som_imx8mm.mk
@@ -4,11 +4,11 @@ VAR_SOM_IMX8MM_SITE = ${BR2_EXTERNAL_EMBEDDED_QUICKSTART_PATH}/package/var_som_i
 VAR_SOM_IMX8MM_INSTALL_TARGET = YES
 
 define VAR_SOM_IMX8MM_INSTALL_BCM43XX_BT
-	$(INSTALL) -D -m 755 $(@D)/bcm43xx-bt $(TARGET_DIR)/bluetooth/variscite-bt.d/bcm43xx-bt
+	$(INSTALL) -D -m 755 $(@D)/bcm43xx-bt $(TARGET_DIR)/etc/bluetooth/variscite-bt.d/bcm43xx-bt
 endef
 
 define VAR_SOM_IMX8MM_INSTALL_BCM43XX_WIFI
-	$(INSTALL) -D -m 755 $(@D)/bcm43xx-wifi $(TARGET_DIR)/wifi/variscite-wifi.d/bcm43xx-wifi
+	$(INSTALL) -D -m 755 $(@D)/bcm43xx-wifi $(TARGET_DIR)/etc/wifi/variscite-wifi.d/bcm43xx-wifi
 endef
 
 VAR_SOM_IMX8MM_POST_INSTALL_TARGET_HOOKS += VAR_SOM_IMX8MM_INSTALL_BCM43XX_BT VAR_SOM_IMX8MM_INSTALL_BCM43XX_WIFI
