cafe397609a69d0c87349d5bae66be84067ad5f8
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-18 17:10:14 +0200

    Improve genimage

--- genimage.sh
+++ genimage.sh
@@ -242,6 +242,32 @@ fi
 
 echo "----------------------------------------------------------"
 
+echo "-------------------- Session ---------------------------"
+
+if [ -f "${EXTRACTED_ROOTFS_HOST_PATH}/usr/bin/login_ng-session" ]; then
+    mkdir -p "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session"
+
+    if [ ! -f "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service" ]; then
+        echo '{' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "kind": "service",' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "cmd": "weston",' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "args": [ "--config=/etc/weston.ini" ],' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "max_restarts": 0,' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "restart_delay_secs": 5,' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '    "dependencies": [  ]' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+        echo '}' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/login_ng-session/default.service"
+    fi
+
+    if [ ! -f "${EXTRACTED_ROOTFS_HOST_PATH}/etc/weston.ini" ]; then
+        echo '[core]' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/weston.ini"
+        echo 'backend=drm' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/weston.ini"
+        echo 'idle-time=0' >> "${EXTRACTED_ROOTFS_HOST_PATH}/etc/weston.ini"
+    fi
+
+fi
+
+echo "----------------------------------------------------------"
+
 echo "------------------- /etc/rdtab ---------------------------"
 
 # since systemd wants to write /etc/machine-id before mounting things in /etc/fstab and missing /etc/machine-id means dbus-broker breaking
