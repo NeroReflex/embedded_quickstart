ff51a531b36b819f3199087b18ea11e0a22ccce2
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-21 14:40:26 +0200

    Creating link

--- meta-embedded_quickstart/build.sh
+++ meta-embedded_quickstart/build.sh
@@ -21,5 +21,8 @@ fi
 echo "Running genimage.sh from $CURRENT_SCRIPT_DIRNAME/sources/embedded_quickstart/genimage.sh"
 sudo bash "$CURRENT_SCRIPT_DIRNAME/sources/embedded_quickstart/genimage.sh" "$CURRENT_SCRIPT_DIRNAME/build-$MACHINE/tmp/deploy/images/$MACHINE" "factory"
 
-rm -f disk_image.img
-ln -sf "$CURRENT_SCRIPT_DIR/build-$MACHINE/tmp/deploy/images/$MACHINE/disk_image.img" "disk_image.img"
+readonly DISK_IMAGE_LINK="$CURRENT_SCRIPT_DIR/disk_image_$MACHINE.img"
+echo "Creating symlink ${DISK_IMAGE_LINK}"
+
+rm -f "${DISK_IMAGE_LINK}"
+ln -sf "$CURRENT_SCRIPT_DIR/build-$MACHINE/tmp/deploy/images/$MACHINE/disk_image.img" "${DISK_IMAGE_LINK}"
