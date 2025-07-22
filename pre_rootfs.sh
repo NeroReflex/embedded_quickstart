dfd49953da47125b159782bc4cc0a56bedb1cbd1
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-22 15:07:14 +0200

    Exclude pam_selinux.so and pam_console.so when they are missing

--- pre_rootfs.sh
+++ pre_rootfs.sh
@@ -5,4 +5,22 @@ if [ -f "${TARGET_DIR}/usr/share/factory/etc/pam.d/system-auth" ] && [ ! -f "${T
     cp "${TARGET_DIR}/usr/share/factory/etc/pam.d/system-auth" "${TARGET_DIR}/etc/pam.d/system-auth"
 fi
 
+for file in /etc/pam.d/*; do
+    pam_selinux_location=$(find "${TARGET_DIR}/usr" -name "pam_selinux.so")
+    if [ -z "$pam_selinux_location" ]; then
+        if [ -f "$file" ]; then
+            sed -i '/pam_selinux.so/s/^/#/' "$file"
+        fi
+    fi
+
+    pam_console_location=$(find "${TARGET_DIR}/usr" -name "pam_console.so")
+    if [ -z "$pam_console_location" ]; then
+        if [ -f "$file" ]; then
+            sed -i '/pam_console.so/s/^/#/' "$file"
+        fi
+    fi
+done
+
+touch "${TARGET_DIR}/etc/pam_debug"
+
 echo "${TARGET_DIR}"
