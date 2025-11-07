47183f9410b6800bcba9a7b6bae92f2d5fef0691
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-11-07 16:46:36 +0100

    Add a (nonworking) boot menu entry

--- genimage.sh
+++ genimage.sh
@@ -152,11 +152,26 @@ elif [ -f "${BINARIES_DIR}/grub-efi-bootx64.efi" ]; then
 
     cp "${CURRENT_SCRIPT_DIR}/refind.conf" "${TARGET_ROOTFS}/EFI/refind/"
 
+    readonly ROOTFS_PARTUUID=$(blkid -s PARTUUID -o value "${LOOPBACK_OUTPUT}p${IMAGE_PART_NUMBER}")
+    readonly ROOTFS_PARTUUID_RESULT=$?
+    if [ $ROOTFS_PARTUUID_RESULT -eq 0 ]; then
+        echo "Configuring rEFInd to start: '${ROOTFS_PARTUUID}'"
+
+        echo 'menuentry "Linux (no initramfs)" {' >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+        echo "    volume PARTUUID=$ROOTFS_PARTUUID" >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+        echo "    loader /@/boot/vmlinuz-5.15.0-custom" >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+        echo "    options \"root=PARTUUID=$ROOTFS_PARTUUID rw rootfstype=btrfs \"" >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+        echo "    icon /EFI/refind/icons/os_linux.png" >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+        echo "}" >> "${TARGET_ROOTFS}/EFI/refind/refind.conf"
+
+    else
+        echo "ERROR: Could not fetch the PARTUUID of the rootfs partition"
+        dismantle
+        exit -1
+    fi
+
     sync
     umount "${TARGET_ROOTFS}"
-
-    echo "Debug NOW"
-    exit 0
 else
     echo "Unsupported hardware."
     dismantle
