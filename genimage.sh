193b7ffe23978d151e33ec18f3d2bb631715839c
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-12-16 16:49:43 +0100

    audit

--- genimage.sh
+++ genimage.sh
@@ -589,6 +589,9 @@ if [ -d "${EXTRACTED_ROOTFS_HOST_PATH}/etc/selinux" ]; then
         exit -1
     fi
 
+    # This is required for audit logs
+    mkdir -p "${EXTRACTED_ROOTFS_HOST_PATH}/var/log/audit"
+
     if setfiles -m -F -r "${EXTRACTED_ROOTFS_HOST_PATH}" "${EXTRACTED_ROOTFS_HOST_PATH}/etc/selinux/${POL_TYPE}/contexts/files/file_contexts" "${EXTRACTED_ROOTFS_HOST_PATH}"; then
         echo "SELinux file contexts applied successfully."
     else
